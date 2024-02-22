df66719fb8e297045b60c5b0450e0f2654d733ba
r4hu1.exe <71373309+r4hu1-exe@users.noreply.github.com>
2024-02-22 19:55:40 +0000

    stringsadded

---
+++ 09strings.js
@@ -0,0 +1,58 @@
+//Use of Back ticks
+
+const Name = "Rahul"
+let Marks = 89
+
+//console.log("Name:" + Marks) ===========> This is old way to write the code
+ 
+
+//This method is called string polletion because it helps us to add the strings in code 
+
+// Example  This can be used as template 
+
+//Main keywords are ${Enter the name os variables}
+
+console.log(`My Name is ${Name} and I got ${Marks} in Maths Test"`)
+
+
+const gameName = new String('Rahul')
+
+console.log(gameName);
+
+console.log(gameName[0])
+
+//To access the prototype 
+
+
+console.log(gameName.__proto__);
+ // You will get the result  {} <=============================
+
+//To get the length the of the strings
+
+//length is METHODS
+//string ===>Methods , you can use the stringname.{method}
+
+
+console.log(gameName.length)
+ 
+//to convert the strings into upercase or lower case
+
+console.log(gameName.toUpperCase())
+
+
+//Different Methods which are used in strings
+
+/*
+
+at()
+
+charAt()
++
+
+
+
+
+
+
+
+*/
