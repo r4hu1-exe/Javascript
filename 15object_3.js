c7d32c8437a805ad87a393254ec8da1e3fec66da
r4hu1.exe <71373309+r4hu1-exe@users.noreply.github.com>
2024-03-10 18:49:36 +0000

    Adding object 3

---
+++ 15object_3.js
@@ -0,0 +1,14 @@
+const SlackUser = {
+    UserId: 9994,
+    Username:"tester45",
+    Email: "breachboy@gmail.com",
+    Admin : "True",
+    courseInstructor:"Hitesh"
+}
+
+//SlackUser.ChannelAdmin
+
+const {courseInstructor:instructor} = course
+
+
+console.log(instructor)
